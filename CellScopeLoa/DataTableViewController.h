98121322cf7b6bcb97178cc9ce9c4c7ab0011163
bakalarmh <matthew.bakalar@gmail.com>
2015-05-30 19:32:37 -0700

    Updated lots of code. Getting close to finished

---
+++ CellScopeLoa/DataTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DataTableViewController.h
+//  CellScopeLoa
+//
+//  Created by Matthew Bakalar on 5/20/15.
+//  Copyright (c) 2015 Fletcher Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataTableViewController : UITableViewController
+
+@end
